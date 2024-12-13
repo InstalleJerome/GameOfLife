64510136d17bc5c08031bdc724b6e5ef7fdc0af1
Installé Jérôme <22237@ecam.be>
2024-12-13 10:45:09 +0100

    Add eat cooldown

--- ViewModels/LivingObject.cs
+++ ViewModels/LivingObject.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using CommunityToolkit.Mvvm.ComponentModel;
 using GameOfLife.ViewModels;
@@ -7,10 +8,21 @@ namespace GameOfLife.ViewModels;
 public abstract partial class Living_Object : GameObject{
     [ObservableProperty]
     private int energy;
-    public Living_Object(Point location, int energy,int health) : base(location, health){
+
+    public const int EatCooldown = 3;
+    public DateTime lastEat{get; set;} = DateTime.Now;
+
+    public bool CanEat {
+        get {
+            return (DateTime.Now-lastEat).TotalSeconds>EatCooldown;
+        }
+    }
+    public Living_Object(Point location, int energy,int health, DateTime lastEat) : base(location, health){
         Health = health;
         Energy = energy;
+        this.lastEat = lastEat;
     }
+    
 
     public abstract void Eat();
     public override void Tick(){
